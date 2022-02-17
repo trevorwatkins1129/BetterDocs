ed01557b6e71b1c8944f9e8c36b2dfb2347c8558
Glitch (glitch-blank-website) <none>
2022-02-17 14:09:50 +0000

    🎇👁 Checkpoint ./index.html:32441189/390 ./style.css:32441189/244 ./script.js:32441189/1916 ./upload.php:32441189/619

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ function save() {
 function load() {
   document.getElementById("text").innerHTML = localStorage.document;
 }
+
+const actualBtn = document.getElementById('actual-btn');
+
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})
+
