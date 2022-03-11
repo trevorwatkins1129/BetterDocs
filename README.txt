d1e78b86c2bce4f899488e0afad35ac0b3ed60a1
Glitch (betterdocs) <none>
2022-03-11 13:42:46 +0000

    ❕🌀 Updated with Glitch

---
+++ README.txt
@@ -0,0 +1,11 @@
+BetterDocs (Version 2.1.8-beta.4)
+
+Developed by Trevor Watkins
+
+Credits:
+
+1) Mahesh, contentEditable html property
+2) W3Schools, MANY pieces of code
+3) 0shuvo0, draw.html script, css, & html
+
+Made with Glitch 
--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+function editorcheck() {
+  const x = document.getElementById("text");
+  if (x.contentEditable == "true") {
+    x.contentEditable = "false";
+  } else {
+    x.contentEditable = "true";
+  }
+}
 
-console.log("Hello, world!");
+function save() {
+  var x = document.getElementById("text").innerHTML;
+  localStorage.document = x;
+}
+
+function load() {
+  document.getElementById("text").innerHTML = localStorage.document;
+}
+
+// Function to download data to a file
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+function downloadprep() {
+  let currentDate = new Date();
+  let cDay = currentDate.getDate()
+  let cMonth = currentDate.getMonth() + 1
+  let cYear = currentDate.getFullYear()
+  var y = document.getElementById("text").innerHTML + "\n " + localStorage.watermark
+  var z = localStorage.filename
+  download(y, z + "-" + cDay + "-" + cMonth + "-" + cYear + "")
+}
+
+function saveWatermark() {
+  localStorage.watermark = document.getElementById("Watermark").value
+}
+
